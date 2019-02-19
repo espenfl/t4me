0ccd332864681a98e67e40416eddad81e7c0fee0
Espen Flage-Larsen <espen.flage-larsen@sintef.no>
2019-02-19 14:50:28 +0100

    Updated tag

--- ops/update_version.py
+++ ops/update_version.py
@@ -4,14 +4,15 @@ import re
 import json
 import fileinput
 import contextlib
-import subprocess32 as subprocess
+import subprocess
 
 from packaging import version
 from py import path as py_path  # pylint: disable=no-name-in-module,no-member
 
 
 def subpath(*args):
-    return os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', *args))
+    return os.path.realpath(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', *args))
 
 
 @contextlib.contextmanager
@@ -24,14 +25,14 @@ def file_input(*args, **kwargs):
         input_fo.close()
 
 
-class VersionUpdater(object):
+class VersionUpdater():
     """
     Version number synchronisation interface.
 
     Updates the version information in
 
     * setup.json
-    * aiida_vasp/__init__.py
+    * src/t4me/__init__.py
 
     to the current version number.
 
@@ -40,18 +41,22 @@ class VersionUpdater(object):
     """
 
     version_pat = re.compile(r'\d+.\d+.\d+')
-    init_version_pat = re.compile(r'(__version__ = )([\'"])(.*)([\'"])', re.DOTALL | re.MULTILINE)
+    init_version_pat = re.compile(r'(__version__ = )([\'"])(.*)([\'"])',
+                                  re.DOTALL | re.MULTILINE)
 
     def __init__(self):
         """Initialize with documents that should be kept up to date and actual version."""
-        self.top_level_init = py_path.local(subpath('aiida_vasp', '__init__.py'))
+        self.top_level_init = py_path.local(
+            subpath('src', 't4me', '__init__.py'))
         self.setup_json = py_path.local(subpath('setup.json'))
         self.version = self.get_version()
 
     def write_to_init(self):
+        """Write version to init."""
         init_content = self.top_level_init.read()
         self.top_level_init.write(
-            re.sub(self.init_version_pat, r'\1\g<2>{}\4'.format(str(self.version)), init_content, re.DOTALL | re.MULTILINE))
+            re.sub(self.init_version_pat, r'\1\g<2>{}\4'.format(
+                str(self.version)), init_content, re.DOTALL | re.MULTILINE))
 
     def write_to_setup(self):
         """Write the updated version number to the setup file."""
@@ -62,21 +67,26 @@ class VersionUpdater(object):
 
     @property
     def setup_version(self):
+        """Fetch version from setup.json."""
         return version.parse(json.load(self.setup_json)['version'])
 
     @property
     def init_version(self):
+        """Fetch version from the init file."""
         match = re.search(self.init_version_pat, self.top_level_init.read())
         if not match:
-            raise AttributeError('No __version__ found in top-level __init__.py')
+            raise AttributeError(
+                'No __version__ found in top-level __init__.py')
         return version.parse(match.groups()[2])
 
     @property
     def tag_version(self):
         """Get the current version number from ``git describe``, fall back to setup.json."""
         try:
-            describe_byte_string = subprocess.check_output(['git', 'describe', '--tags', '--match', 'v*.*.*'])
-            version_string = re.findall(self.version_pat, describe_byte_string)[0]
+            describe_byte_string = subprocess.check_output(
+                ['git', 'describe', '--tags', '--match', 'v*.*.*'])
+            version_string = re.findall(
+                self.version_pat, describe_byte_string.decode('utf-8'))[0]
         except subprocess.CalledProcessError:
             with open(self.setup_json.strpath, 'r') as setup_fo:
                 setup = json.load(setup_fo)
